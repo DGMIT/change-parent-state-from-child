c9134f0b4c7b2cf0f0b172f808dc1d803490e559
ArchieKwon <ceo@dgmit.com>
2018-11-30 11:25:01 +0900

    change button text

--- Child.js
+++ Child.js
@@ -11,12 +11,12 @@ export default class Child extends Component<Props> {
       <View>
       <TouchableOpacity onPress={()=>this.props.onPress(this.props.seed + 10)}>
         <View style={{ width: 200, height: 100, borderRadius: 10, borderWidth: 1, padding: 10, }}>
-            <Text style={{ fontSize: 20, textAlign: 'center'}}>PUSH TO +10 TO PARENT's STATE VALUE</Text>
+            <Text style={{ fontSize: 20, textAlign: 'center'}}>ADD 10 TO PARENT's STATE VALUE</Text>
         </View>
       </TouchableOpacity>
       <TouchableOpacity onPress={()=>this.props.onPress(this.props.seed - 10)}>
         <View style={{ width: 200, height: 100, borderRadius: 10, borderWidth: 1, padding: 10, }}>
-            <Text style={{ fontSize: 20, textAlign: 'center'}}>PUSH TO -10 TO PARENT's STATE VALUE</Text>
+            <Text style={{ fontSize: 20, textAlign: 'center'}}>SUBSTRACT 10 TO PARENT's STATE VALUE</Text>
         </View>
       </TouchableOpacity>
       </View>
