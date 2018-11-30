f848722e8db339210a0168460e0a598e9ae42982
ArchieKwon <ceo@dgmit.com>
2018-11-30 11:21:18 +0900

    Add substract function

--- Child.js
+++ Child.js
@@ -8,11 +8,18 @@ type Props = {};
 export default class Child extends Component<Props> {
   render() {
     return (
+      <View>
       <TouchableOpacity onPress={()=>this.props.onPress(this.props.seed + 10)}>
         <View style={{ width: 200, height: 100, borderRadius: 10, borderWidth: 1, padding: 10, }}>
-            <Text style={{ fontSize: 20, textAlign: 'center'}}>PUSH TO ADD +10 TO PARENT's STATE VALUE</Text>
+            <Text style={{ fontSize: 20, textAlign: 'center'}}>PUSH TO +10 TO PARENT's STATE VALUE</Text>
         </View>
       </TouchableOpacity>
+      <TouchableOpacity onPress={()=>this.props.onPress(this.props.seed - 10)}>
+        <View style={{ width: 200, height: 100, borderRadius: 10, borderWidth: 1, padding: 10, }}>
+            <Text style={{ fontSize: 20, textAlign: 'center'}}>PUSH TO -10 TO PARENT's STATE VALUE</Text>
+        </View>
+      </TouchableOpacity>
+      </View>
     );
   }
 }
